439a1f143bfe2c3c88be7baa78841c29ab18772a
rahul <rahulpharidas220#gmail.com>
2021-09-28 21:21:48 +0530

    28/09/2021

--- src/HodRoutes.js
+++ src/HodRoutes.js
@@ -1,42 +1,42 @@
 // @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
 import Notifications from "@material-ui/icons/Notifications";
-import ViewCarouselIcon from "@material-ui/icons/ViewCarousel";
-import PeopleIcon from "@material-ui/icons/People";
-import DateRangeIcon from "@material-ui/icons/DateRange";
-import CommentIcon from "@material-ui/icons/Comment";
-import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+// import ViewCarouselIcon from "@material-ui/icons/ViewCarousel";
+// import PeopleIcon from "@material-ui/icons/People";
+// import DateRangeIcon from "@material-ui/icons/DateRange";
+// import CommentIcon from "@material-ui/icons/Comment";
+// import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import FileCopyIcon from "@material-ui/icons/FileCopy";
 import ListIcon from "@material-ui/icons/List";
 import MenuBookIcon from "@material-ui/icons/MenuBook";
 import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
-import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
-import BusinessIcon from "@material-ui/icons/Business";
-import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
-import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+// import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+// import BusinessIcon from "@material-ui/icons/Business";
+// import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+// import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
 import VerticalSplitIcon from "@material-ui/icons/VerticalSplit";
-import AssignmentIcon from "@material-ui/icons/Assignment";
-import EmailIcon from "@material-ui/icons/Email";
+// import AssignmentIcon from "@material-ui/icons/Assignment";
+// import EmailIcon from "@material-ui/icons/Email";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
-import Slideshow from "views/Slideshow/Slideshow";
-import Events from "views/Events/Events";
-import Gallery from "views/Gallery/Gallery";
+// import Slideshow from "views/Slideshow/Slideshow";
+// import Events from "views/Events/Events";
+// import Gallery from "views/Gallery/Gallery";
 import Courses from "views/Courses/Courses";
-import Testimonials from "views/Testimonials/Testimonials";
+// import Testimonials from "views/Testimonials/Testimonials";
 import Students from "views/Students/Students";
-import Publications from "views/Publications/Publications";
+// import Publications from "views/Publications/Publications";
 import Subjects from "views/Subjects/Subjects";
 import Semester from "views/Semester/Semester";
 import InternalMarks from "views/InternalMarks/InternalMarks";
 import InternalsNotification from "views/InternalsNotification/InternalsNotification";
-import Placements from "views/Placements/Placements";
-import Enquiry from "views/Enquiry/Enquiry";
+// import Placements from "views/Placements/Placements";
+// import Enquiry from "views/Enquiry/Enquiry";
 
-import Clubs from "views/Clubs/Clubs";
-import Facilities from "views/Facilities/Facilities";
-import Management from "views/Management/Management";
-import Staffs from "views/Sfatts/Staffs";
+// import Clubs from "views/Clubs/Clubs";
+// import Facilities from "views/Facilities/Facilities";
+// import Management from "views/Management/Management";
+// import Staffs from "views/Sfatts/Staffs";
 // core components/views for RTL layout
 
 const dashboardRoutes = [
@@ -47,41 +47,41 @@ const dashboardRoutes = [
     component: DashboardPage,
     layout: "/admin",
   },
-  {
-    path: "/staffs",
-    name: "Manage Staffs",
-    icon: PeopleIcon,
-    component: Staffs,
-    layout: "/admin",
-  },
-  {
-    path: "/slideshow",
-    name: "Home screen Carousel",
-    icon: ViewCarouselIcon,
-    component: Slideshow,
-    layout: "/admin",
-  },
-  {
-    path: "/events",
-    name: "Events",
-    icon: DateRangeIcon,
-    component: Events,
-    layout: "/admin",
-  },
-  {
-    path: "/testimonials",
-    name: "Testimonials",
-    icon: CommentIcon,
-    component: Testimonials,
-    layout: "/admin",
-  },
-  {
-    path: "/gallery",
-    name: "Gallery",
-    icon: PhotoLibraryIcon,
-    component: Gallery,
-    layout: "/admin",
-  },
+  // {
+  //   path: "/staffs",
+  //   name: "Manage Staffs",
+  //   icon: PeopleIcon,
+  //   component: Staffs,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/slideshow",
+  //   name: "Home screen Carousel",
+  //   icon: ViewCarouselIcon,
+  //   component: Slideshow,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/events",
+  //   name: "Events",
+  //   icon: DateRangeIcon,
+  //   component: Events,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/testimonials",
+  //   name: "Testimonials",
+  //   icon: CommentIcon,
+  //   component: Testimonials,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/gallery",
+  //   name: "Gallery",
+  //   icon: PhotoLibraryIcon,
+  //   component: Gallery,
+  //   layout: "/admin",
+  // },
 
   {
     path: "/courses",
@@ -111,34 +111,34 @@ const dashboardRoutes = [
     component: Students,
     layout: "/admin",
   },
-  {
-    path: "/clubs",
-    name: "Clubs",
-    icon: CalendarViewDayIcon,
-    component: Clubs,
-    layout: "/admin",
-  },
-  {
-    path: "/facilities",
-    name: "Facilities",
-    icon: BusinessIcon,
-    component: Facilities,
-    layout: "/admin",
-  },
-  {
-    path: "/management",
-    name: "Management",
-    icon: BusinessCenterIcon,
-    component: Management,
-    layout: "/admin",
-  },
-  {
-    path: "/publications",
-    name: "Publications",
-    icon: LibraryBooksIcon,
-    component: Publications,
-    layout: "/admin",
-  },
+  // {
+  //   path: "/clubs",
+  //   name: "Clubs",
+  //   icon: CalendarViewDayIcon,
+  //   component: Clubs,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/facilities",
+  //   name: "Facilities",
+  //   icon: BusinessIcon,
+  //   component: Facilities,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/management",
+  //   name: "Management",
+  //   icon: BusinessCenterIcon,
+  //   component: Management,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/publications",
+  //   name: "Publications",
+  //   icon: LibraryBooksIcon,
+  //   component: Publications,
+  //   layout: "/admin",
+  // },
   {
     path: "/Internalsnotification",
     name: "Internals Notification",
@@ -153,20 +153,20 @@ const dashboardRoutes = [
     component: InternalMarks,
     layout: "/admin",
   },
-  {
-    path: "/placements",
-    name: "Placements",
-    icon: AssignmentIcon,
-    component: Placements,
-    layout: "/admin",
-  },
-  {
-    path: "/enquiry",
-    name: "Enquiry",
-    icon: EmailIcon,
-    component: Enquiry,
-    layout: "/admin",
-  },
+  // {
+  //   path: "/placements",
+  //   name: "Placements",
+  //   icon: AssignmentIcon,
+  //   component: Placements,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/enquiry",
+  //   name: "Enquiry",
+  //   icon: EmailIcon,
+  //   component: Enquiry,
+  //   layout: "/admin",
+  // },
 ];
 
 export default dashboardRoutes;
--- src/StaffRoutes.js
+++ src/StaffRoutes.js
@@ -1,39 +1,39 @@
 // @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
-import Notifications from "@material-ui/icons/Notifications";
-import ViewCarouselIcon from "@material-ui/icons/ViewCarousel";
+// import Notifications from "@material-ui/icons/Notifications";
+// import ViewCarouselIcon from "@material-ui/icons/ViewCarousel";
 
-import DateRangeIcon from "@material-ui/icons/DateRange";
-import CommentIcon from "@material-ui/icons/Comment";
-import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
-import FileCopyIcon from "@material-ui/icons/FileCopy";
-import ListIcon from "@material-ui/icons/List";
-import MenuBookIcon from "@material-ui/icons/MenuBook";
+// import DateRangeIcon from "@material-ui/icons/DateRange";
+// import CommentIcon from "@material-ui/icons/Comment";
+// import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+// import FileCopyIcon from "@material-ui/icons/FileCopy";
+// import ListIcon from "@material-ui/icons/List";
+// import MenuBookIcon from "@material-ui/icons/MenuBook";
 import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
-import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
-import BusinessIcon from "@material-ui/icons/Business";
-import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
-import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+// import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+// import BusinessIcon from "@material-ui/icons/Business";
+// import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+// import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
 import VerticalSplitIcon from "@material-ui/icons/VerticalSplit";
-import AssignmentIcon from "@material-ui/icons/Assignment";
+// import AssignmentIcon from "@material-ui/icons/Assignment";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.js";
-import Slideshow from "views/Slideshow/Slideshow";
-import Events from "views/Events/Events";
-import Gallery from "views/Gallery/Gallery";
-import Courses from "views/Courses/Courses";
-import Testimonials from "views/Testimonials/Testimonials";
+// import Slideshow from "views/Slideshow/Slideshow";
+// import Events from "views/Events/Events";
+// import Gallery from "views/Gallery/Gallery";
+// import Courses from "views/Courses/Courses";
+// import Testimonials from "views/Testimonials/Testimonials";
 import Students from "views/Students/Students";
-import Publications from "views/Publications/Publications";
-import Subjects from "views/Subjects/Subjects";
-import Semester from "views/Semester/Semester";
+// import Publications from "views/Publications/Publications";
+// import Subjects from "views/Subjects/Subjects";
+// import Semester from "views/Semester/Semester";
 import InternalMarks from "views/InternalMarks/InternalMarks";
-import InternalsNotification from "views/InternalsNotification/InternalsNotification";
-import Placements from "views/Placements/Placements";
+// import InternalsNotification from "views/InternalsNotification/InternalsNotification";
+// import Placements from "views/Placements/Placements";
 
-import Clubs from "views/Clubs/Clubs";
-import Facilities from "views/Facilities/Facilities";
-import Management from "views/Management/Management";
+// import Clubs from "views/Clubs/Clubs";
+// import Facilities from "views/Facilities/Facilities";
+// import Management from "views/Management/Management";
 // core components/views for RTL layout
 
 const dashboardRoutes = [
@@ -44,55 +44,55 @@ const dashboardRoutes = [
     component: DashboardPage,
     layout: "/admin",
   },
-  {
-    path: "/slideshow",
-    name: "Home screen Carousel",
-    icon: ViewCarouselIcon,
-    component: Slideshow,
-    layout: "/admin",
-  },
-  {
-    path: "/events",
-    name: "Events",
-    icon: DateRangeIcon,
-    component: Events,
-    layout: "/admin",
-  },
-  {
-    path: "/testimonials",
-    name: "Testimonials",
-    icon: CommentIcon,
-    component: Testimonials,
-    layout: "/admin",
-  },
-  {
-    path: "/gallery",
-    name: "Gallery",
-    icon: PhotoLibraryIcon,
-    component: Gallery,
-    layout: "/admin",
-  },
-  {
-    path: "/courses",
-    name: "Courses",
-    icon: FileCopyIcon,
-    component: Courses,
-    layout: "/admin",
-  },
-  {
-    path: "/semester",
-    name: "Semester",
-    icon: ListIcon,
-    component: Semester,
-    layout: "/admin",
-  },
-  {
-    path: "/subjects",
-    name: "Subjects",
-    icon: MenuBookIcon,
-    component: Subjects,
-    layout: "/admin",
-  },
+  // {
+  //   path: "/slideshow",
+  //   name: "Home screen Carousel",
+  //   icon: ViewCarouselIcon,
+  //   component: Slideshow,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/events",
+  //   name: "Events",
+  //   icon: DateRangeIcon,
+  //   component: Events,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/testimonials",
+  //   name: "Testimonials",
+  //   icon: CommentIcon,
+  //   component: Testimonials,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/gallery",
+  //   name: "Gallery",
+  //   icon: PhotoLibraryIcon,
+  //   component: Gallery,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/courses",
+  //   name: "Courses",
+  //   icon: FileCopyIcon,
+  //   component: Courses,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/semester",
+  //   name: "Semester",
+  //   icon: ListIcon,
+  //   component: Semester,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/subjects",
+  //   name: "Subjects",
+  //   icon: MenuBookIcon,
+  //   component: Subjects,
+  //   layout: "/admin",
+  // },
   {
     path: "/students",
     name: "Students",
@@ -100,41 +100,41 @@ const dashboardRoutes = [
     component: Students,
     layout: "/admin",
   },
-  {
-    path: "/clubs",
-    name: "Clubs",
-    icon: CalendarViewDayIcon,
-    component: Clubs,
-    layout: "/admin",
-  },
-  {
-    path: "/facilities",
-    name: "Facilities",
-    icon: BusinessIcon,
-    component: Facilities,
-    layout: "/admin",
-  },
-  {
-    path: "/management",
-    name: "Management",
-    icon: BusinessCenterIcon,
-    component: Management,
-    layout: "/admin",
-  },
-  {
-    path: "/publications",
-    name: "Publications",
-    icon: LibraryBooksIcon,
-    component: Publications,
-    layout: "/admin",
-  },
-  {
-    path: "/Internalsnotification",
-    name: "Internals Notification",
-    icon: Notifications,
-    component: InternalsNotification,
-    layout: "/admin",
-  },
+  // {
+  //   path: "/clubs",
+  //   name: "Clubs",
+  //   icon: CalendarViewDayIcon,
+  //   component: Clubs,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/facilities",
+  //   name: "Facilities",
+  //   icon: BusinessIcon,
+  //   component: Facilities,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/management",
+  //   name: "Management",
+  //   icon: BusinessCenterIcon,
+  //   component: Management,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/publications",
+  //   name: "Publications",
+  //   icon: LibraryBooksIcon,
+  //   component: Publications,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/Internalsnotification",
+  //   name: "Internals Notification",
+  //   icon: Notifications,
+  //   component: InternalsNotification,
+  //   layout: "/admin",
+  // },
   {
     path: "/internals",
     name: "Internal Marks",
@@ -142,13 +142,13 @@ const dashboardRoutes = [
     component: InternalMarks,
     layout: "/admin",
   },
-  {
-    path: "/placements",
-    name: "Placements",
-    icon: AssignmentIcon,
-    component: Placements,
-    layout: "/admin",
-  },
+  // {
+  //   path: "/placements",
+  //   name: "Placements",
+  //   icon: AssignmentIcon,
+  //   component: Placements,
+  //   layout: "/admin",
+  // },
 ];
 
 export default dashboardRoutes;
